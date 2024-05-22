cba63d28268c28599891a3cd13aaa1b1ff97369b
parkdongju <pdj95044@gmail.com>
2024-05-22 10:44:58 +0900

    thread

--- thread/Mutex_Cond.c
+++ thread/Mutex_Cond.c
@@ -1,4 +1,15 @@
 #include <pthread.h>
+/*
+pthread_cond_wait(cond, mutex);
+해당 함수가 실행된 스레드의 동작을 일시정지 시키는 함수입니다.
+
+pthread_cond_signal(cond);
+pthread_cond_wait를 통해 일시정지된 스레드중에 하나를 재실행 시키는 함수입니다.
+
+pthread_cond_broadcast(cond);
+pthread_cond_wait를 통해 일시정지된 스레드들을 모두 재실행 시키는 함수입니다.
+
+*/
 #include <stdio.h>
 #include <unistd.h>
 
@@ -7,9 +18,9 @@ pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 void *counter1(void *arg) {
     for (int i = 0; i < 10; i++) {   
-        if (i == 2 || i == 5) {
+        if (i == 2) {
             pthread_mutex_lock(&mutex1);
-            pthread_cond_signal(&cond1);
+            pthread_cond_broadcast(&cond1);
             pthread_mutex_unlock(&mutex1);
         }
         printf("thread1 : %d\n", i);
--- thread/thread.c
+++ thread/thread.c
@@ -1,65 +1,39 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<unistd.h>
-#include<time.h>
+/*
+pthread_create()
+스레드를 생성하는 함수입니다.
 
-void *counter1(){
-    for (int i = 0; i < 10; i++)
-    {
-        time_t current_time;
-        struct tm *local_time;
+pthread_exit()
+스레드를 종료하는 함수입니다. 종료 이후에 인자값을 pthread_join의 두번째 인자로 전달이 됩니다.
 
-        // 현재 시간 가져오기
-        time(&current_time);
+pthread_join()
+스레드의 종료 시점을 기다리는 함수입니다. pthread_exit으로부터 
 
-        // 현재 시간을 로컬 시간으로 변환
-        local_time = localtime(&current_time);
+*/
 
-        printf("counter1 : %d | 현재 시간: %d-%02d-%02d %02d:%02d:%02d\n",
-            i,
-            local_time->tm_year + 1900, // tm_year는 1900년을 기준으로 하기 때문에 1900을 더합니다.
-            local_time->tm_mon + 1,     // tm_mon은 0부터 시작하기 때문에 1을 더합니다.
-            local_time->tm_mday,
-            local_time->tm_hour,
-            local_time->tm_min,
-            local_time->tm_sec
-        );
-        // fflush(stdout);
-        sleep(1);
-    }
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void *thread_func(void *arg) {
+    // 쓰레드 작업 수행
+    sleep(2);
+
+    // 쓰레드 종료 시 반환할 값 설정
+    pthread_exit((void *)42);
 }
 
-void *counter2(){
-    for (int i = 0; i < 10; i++)
-    {
-        time_t current_time;
-        struct tm *local_time;
+int main() {
+    pthread_t thread;
+    void *exit_status;
 
-        // 현재 시간 가져오기
-        time(&current_time);
+    // 쓰레드 생성
+    pthread_create(&thread, NULL, thread_func, NULL);
 
-        // 현재 시간을 로컬 시간으로 변환
-        local_time = localtime(&current_time);
+    // 쓰레드 종료 대기 및 반환값 저장
+    pthread_join(thread, &exit_status);
 
-        printf("counter2 : %d | 현재 시간: %d-%02d-%02d %02d:%02d:%02d\n",
-            i,
-            local_time->tm_year + 1900, // tm_year는 1900년을 기준으로 하기 때문에 1900을 더합니다.
-            local_time->tm_mon + 1,     // tm_mon은 0부터 시작하기 때문에 1을 더합니다.
-            local_time->tm_mday,
-            local_time->tm_hour,
-            local_time->tm_min,
-            local_time->tm_sec
-        );
-        // fflush(stdout);
-        sleep(1);
-    }
-}
+    // 쓰레드 종료 상태 출력
+    printf("Thread exit status: %ld\n", (long)exit_status);
 
-int main(){
-    pthread_t cnter1, cnter2;
-    pthread_create(&cnter1, NULL, counter1,NULL);
-    pthread_create(&cnter2, NULL, counter2,NULL);
-
-    pthread_join(cnter1,NULL);
-    pthread_join(cnter2,NULL);
+    return 0;
 }
